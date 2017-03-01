496caaebc74e57037795005ee1cd4f4abcf15147
Marilu Llamocca <maryleo.3007@gmail.com>
2017-03-01 12:19:45 -0500

    modificando ejercicio2

--- js/archivo.js
+++ js/archivo.js
@@ -1,1 +1,7 @@
-alert("hola");
+var nombre = document.getElementById('nombre');
+var peso = document.getElementById('peso');
+var estatura = document.getElementById('estatura');
+function calcularImc() {
+  var resultado = peso.value * estatura.value;
+  alert(""+nombre.value+" tu IMC es: "+resultado);
+}
